7da30200db6b73c877846baf95bb72393b0d6e35
nakhoa <nakhoa17@gmail.com>
2018-07-13 01:41:18 +0700

    Return demo data controller index

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Tour;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index(){
-        return View('page.index');
+        $outstandingTour = Tour::take(10)->get();
+        return $outstandingTour;
     }
     
     public function show(){
