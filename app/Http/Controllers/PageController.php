81e57100490d49de0141908fcb5d06c02c187faa
nakhoa <nakhoa17@gmail.com>
2018-07-13 22:39:13 +0700

    Fix controller return views

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,8 +8,23 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function index(){
-        $outstandingTour = Tour::take(10)->get();
-        return $outstandingTour;
+        $outstandingTour = Tour::orderBy('booked', 'DESC')
+                                ->take(10)
+                                ->get();
+
+        $recentlyTour = Tour::orderBy('created_at', 'DESC')
+                            ->take(10)
+                            ->get();
+        
+        $discountTours = Tour::orderBy('created_at', 'DESC')
+                            ->take(10)
+                            ->get();
+
+        return view("page/index", [
+                    "outstanding_tour" => $outstandingTour,
+                    "recently_tour" => $recentlyTour,
+                    "discount_tour" => $discountTours
+                    ]);
     }
     
     public function show(){
