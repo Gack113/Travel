d147159ec69a124c85ff9bd4f2e5612421ed4eff
Gack113 <kugack@gmail.com>
2018-07-16 13:49:17 +0700

    Modify lock file

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -6,13 +6,6 @@
       0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
     ),
   ),
-  'laravel/passport' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Passport\\PassportServiceProvider',
-    ),
-  ),
   'laravel/tinker' => 
   array (
     'providers' => 
